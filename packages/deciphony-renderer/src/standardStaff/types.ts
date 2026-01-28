51039ce02a1a0e68319573872d34eadec5050ea0
liam <2217356256@qq.com>
2026-01-28 21:54:17 +0800

    规范类型命名

--- packages/deciphony-renderer/src/standardStaff/types.ts
+++ packages/deciphony-renderer/src/standardStaff/types.ts
@@ -27,6 +27,7 @@ export type Measure = {
 export type NoteSymbol = {
     type: NoteSymbolTypeEnum
     affiliatedSymbols: AffiliatedSymbol[]
+    region: number // 在五线谱上的位置，0就是第一间的位置，1是第二线的位置
 }
 // 音符附属型符号 accent above, accidental等等， 符干符尾不属于附属型符号，它是根据音符时值信息固定逻辑判断是否存在的符号
 export type AffiliatedSymbol = {
